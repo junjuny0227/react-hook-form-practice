b5f8e6444d125be50215a8954a17199b07437a5f
junjuny0227 <junjuny0227@gmail.com>
2025-05-08 23:25:04 +0900

    공식 문서 코드

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,37 @@
+"use client";
+
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface Inputs {
+  example: string;
+  exampleRequired: string;
+}
+
 const Main = () => {
-  return <div>Main</div>;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  console.log(watch("example")); // watch input value by passing the name of it
+
+  return (
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* register your input into the hook by invoking the "register" function */}
+      <input defaultValue="test" {...register("example")} />
+
+      {/* include validation with required or other standard HTML validation rules */}
+      <input {...register("exampleRequired", { required: true })} />
+      {/* errors will return when field validation fails  */}
+      {errors.exampleRequired && <span>This field is required</span>}
+
+      <input type="submit" />
+    </form>
+  );
 };
 
 export default Main;

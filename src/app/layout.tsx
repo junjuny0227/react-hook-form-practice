b1f29676d0e6816259941d1a2044f946ec699b80
junjuny0227 <junjuny0227@gmail.com>
2025-05-08 22:21:53 +0900

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "react-hook-form & zod",
+  description: "react-hook-form & zod",
+};
+
+const RootLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
